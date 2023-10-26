77bc57bd5122244360f54324d52731c52a9333df
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-10-26 13:30:42 +0300

    Implemented a different solution that generates all possible permutations from the available letters and checks every single possibility for validity. It is too slow and does not take into account the instances where a word contains two occurunces of the same letter.

--- search.py
+++ search.py
@@ -1,21 +1,44 @@
 import requests
-import string
+import re
+from itertools import permutations
+import time
 
-valids = list("qweyuipfhjkzxm")
-base = "ie"
-answer = base
+print("Initializing...")
+t1 = time.time()
 
-for i in valids:
-    s4 = i
-    for j in valids:
-        s5 = j
-        answer = s4 + answer + s5 + "l"
-        re = requests.post("https://words.dev-apis.com/validate-word",json={"word": answer}, timeout=0.8)
-        if(re.json()["validWord"]):
-            print(f"VALID WORD FOUND: {answer}")
-            answer = base #Reset and start over
-        else:
-            print(f"{answer} tried.. Not a valid word!")
-            answer = base #Reset and start over
-            
-print("Finished!")
+# Empty list of possible answers
+answers = []
+
+# The letters available
+valid_letters = [* "qweyuipfhjkzxm"]
+
+# The current pattern in regex notation
+current_pattern = ".i..e"
+
+# All the available permutations of available characters
+char_perms = [* permutations(valid_letters, 5)]
+
+# All permutations that match the current pattern
+filtered_char_perms = [* filter(lambda x: re.search(current_pattern, "".join(x)), char_perms)]
+possible_strings = ["".join(x) for x in filtered_char_perms]
+
+print(f"Initialization complete in {time.time() - t1} seconds. \nNow searching for the possible answers...")
+
+t2 = time.time()
+# Get all the valid words among them
+for possibility in possible_strings:
+    # Check if this is a valid word
+    response = requests.post("https://words.dev-apis.com/validate-word",
+                             json = {"word": possibility},
+                             timeout = 0.8)
+    if response.json()['validWord'] == True:
+        # if it is a valid word, add it to the list of candidate answers
+        answers.append(possibility)
+t3 = time.time
+
+print("The possible answers are:\n")
+print(*answers, sep="\n")
+print(f"Search finished in {t3 - t2} seconds.")
+
+
+## Problems with this script: Takes way too long in search, and doesn't include instances with double letters
